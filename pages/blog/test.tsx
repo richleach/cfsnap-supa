d621b36cf4764f53eed11e83242c0d34c84bb248
Rich Leach <rich@cfsnap.com>
2023-04-10 11:57:08 -0600

    Delete test.tsx

--- pages/blog/test.tsx
+++
@@ -1,6 +0,0 @@
-import {supabase} from '../../components/SupabaseClient';
-
-export default async function Posts() {
-    const { data } = await supabase.from('posts').select() 
-    return <pre>{JSON.stringify(data, null, 2)}</pre>
-}
