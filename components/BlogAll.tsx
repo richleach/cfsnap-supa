2c8668183c14e69929c9a1cb12dc33157b62763e
Rich Leach <rich@cfsnap.com>
2023-04-10 11:50:32 -0600

    updated includes in response to build errors

--- components/BlogAll.tsx
+++ components/BlogAll.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
-import {supabase} from '../../components/SupabaseClient'
+//import {supabase} from '../../components/SupabaseClient'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../../components/Footer'
+//import Footer from '../../components/Footer'
 import Link from 'next/link'
-import { Categories } from '../../components'
+//import { Categories } from '../../components'
 import moment from 'moment';
+import { supabase } from './SupabaseClient'
+import Categories from './Categories'
+import Footer from './Footer'
 
 const Blog: NextPage = () => {
 
@@ -25,36 +28,36 @@ const Blog: NextPage = () => {
   }, [])
 console.log(postData)
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container px-10 mx-auto mb-8">
 
       <Head>
         <title>cfsnap.com: Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='grid grid-cols-1 md:grid-cols-12 gap-12'>
-        <div className='sm:col-span-8 col-span-1'>
-          <div className='bg-white shadow-lg rounded-lg p-4 pb-12 mb-8'>
+      <div className='grid grid-cols-1 gap-12 md:grid-cols-12'>
+        <div className='col-span-1 sm:col-span-8'>
+          <div className='p-4 pb-12 mb-8 bg-white rounded-lg shadow-lg'>
             <div className='grid grid-cols-1 gap-4 align-middle'>
-              <div className="inline-block align-middle bg-white shadow-lg rounded-lg p-0 sm:p-2 pb-0 mb-1 ">
+              <div className="inline-block p-0 pb-0 mb-1 align-middle bg-white rounded-lg shadow-lg sm:p-2 ">
                   
-                  <p className='text-md md:text-3xl p-2 pb-0 mb-0 font-semibold text-center sm:align-middle'>Blog Posts</p>
+                  <p className='p-2 pb-0 mb-0 font-semibold text-center text-md md:text-3xl sm:align-middle'>Blog Posts</p>
                   
               </div>
               
-              <div className="inline-block bg-white shadow-lg rounded-lg p-0 sm:p-8 pb-0 mb-1 align-middle">
+              <div className="inline-block p-0 pb-0 mb-1 align-middle bg-white rounded-lg shadow-lg sm:p-8">
                 {
                     postData.map((post:any) => (
-                        <div className="pb-2 pt-3  sm:pl-2 border-b" key={post.id}>
+                        <div className="pt-3 pb-2 border-b sm:pl-2" key={post.id}>
                             <Link href={post.url}>
-                                <div className='flex items-center justify-center mb-4 lg:mb-2 w-full lg:w-auto'>
-                                    <span className="align-middle text-sm">{moment(post.created_at).format('MMM DD, YYYY')}</span>
+                                <div className='flex items-center justify-center w-full mb-4 lg:mb-2 lg:w-auto'>
+                                    <span className="text-sm align-middle">{moment(post.created_at).format('MMM DD, YYYY')}</span>
                                 </div> 
 
-                                <h2 className='text-md md:text-2xl p-2 pb-0 mb-0 font-semibold text-center sm:align-middle'>{post.headline}</h2>
+                                <h2 className='p-2 pb-0 mb-0 font-semibold text-center text-md md:text-2xl sm:align-middle'>{post.headline}</h2>
                                 <p className="p-3">{post.summary}</p>
-                                <div className='flex items-center justify-center mb-4 lg:mb-2 w-full lg:w-auto'>
-                                    <span className='inline-block bg-pink-600 text-sm font-medium rounded-full text-white px-2 py-1 cursor-pointer'>Continue reading....</span>
+                                <div className='flex items-center justify-center w-full mb-4 lg:mb-2 lg:w-auto'>
+                                    <span className='inline-block px-2 py-1 text-sm font-medium text-white bg-pink-600 rounded-full cursor-pointer'>Continue reading....</span>
                                 </div>
                             </Link>
                         </div>
@@ -68,12 +71,12 @@ console.log(postData)
           </div>
           
         </div>
-        <div className='sm:col-span-4 col-span-1'>
-            <div className='sm:sticky relative top-8'>
+        <div className='col-span-1 sm:col-span-4'>
+            <div className='relative sm:sticky top-8'>
 
               <div className="p-8 pb-12 mb-8 bg-white rounded-lg shadow-lg">
                   <h3 className="pb-4 mb-2 text-xl font-semibold border-b">Blog Categories</h3>
-                  <Categories postProps={postData} />
+                  <Categories />
                   
               </div>
 
