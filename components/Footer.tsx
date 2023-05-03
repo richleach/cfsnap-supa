1992a88de9e710969e3eaccfc134604ff31c4a0d
Rich Leach <rich@cfsnap.com>
2023-05-02 18:43:38 -0600

    formatting for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     
     return (
         <footer className="inline-block w-full px-2 text-center text-blue-900 border-t border-blue-900">
-            <br /><Link href={`/availability/`}>Availability</Link> &nbsp;Copyright &copy; {getYear()} cfsnap.com.  All Rights Reserved.
+            <br /><Link href={`/availability/`}>Availability</Link> | &nbsp;Copyright &copy; {getYear()} cfsnap.com.  All Rights Reserved.
         </footer>
     )
 }
