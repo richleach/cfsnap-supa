33288d2dfab23999ac2dac44222261ab047672a7
Rich Leach <rich@cfsnap.com>
2024-02-25 15:50:05 -0700

    added font code to use google font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,33 @@
-import '../styles/globals.css'
-import { useEffect, useState } from 'react';
-import { Layout } from '../components/'
-import 'tailwindcss/tailwind.css';
-import '../styles/globals.scss';
-import type { AppProps } from 'next/app'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
+import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import { Layout } from "../components/";
+import "tailwindcss/tailwind.css";
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
+import { Montserrat } from "next/font/google";
 
-function MyApp({ Component, pageProps,  }: AppProps<{
-  initialSession: Session
+const monts = Montserrat({ subsets: ["latin"] });
+
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session;
 }>) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
-  
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+
   return (
     <SessionContextProvider
       supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}>
-      <Layout>
+      initialSession={pageProps.initialSession}
+    >
+      <Layout className={monts.className}>
         <Component {...pageProps} />
       </Layout>
     </SessionContextProvider>
-
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
